a3a9cfeaa890c83df58e758fdc8dd11e759b034c
therealcarvv <isaccarv009@gmail.com>
2026-09-14 21:10:20 -0300

    padacertu
    mudeiaiprarodá

--- Jogo/backend/src/Zombie.java
+++ Jogo/backend/src/Zombie.java
@@ -8,7 +8,7 @@ public class Zombie {
     public Zombie(int x, int y) {
         this.x = x;
         this.y = y;
-        this.velocidade = 10;
+        this.velocidade = 20;
     }
 
     int getX() {
